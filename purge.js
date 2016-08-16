f491d59ff8f8fadc01a313fd1e798c3c7d5ca24c
Urban Sanden <hej@urre.me>
2016-08-16 19:35:07 +0200

    Minor

--- purge.js
+++
@@ -1,8 +0,0 @@
-var config = require('./secrets.js').cloudflare;
-
-require('cloudflare-purge').purgeFile({
-	zone: config.cloudflare.zoneid,
-	file: config.cloudflare.domain,
-	email: config.cloudflare.email,
-	key: config.cloudflare.token,
-});
